e69694095dfed62ac15f3effcd52c2d727aef16d
DiogoApenaaas <125512709+DiogoApenaaas@users.noreply.github.com>
2024-06-21 08:21:17 -0300

    dasdasdas

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Olá, seja bem vindo! Este site será utilizado para um melhor vizualização do metodo RESPONSIVO.";
-  }
-  let slideIndex = 0;
-showSlides();
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
+    form.addEventListener("submit", function(event) {
+        // Exemplo de validação simples
+        const nome = document.getElementById("nome").value;
+        const email = document.getElementById("email").value;
+
+        if (nome === "" || email === "") {
+            alert("Por favor, preencha todos os campos.");
+            event.preventDefault();
+        }
+    });
+});
